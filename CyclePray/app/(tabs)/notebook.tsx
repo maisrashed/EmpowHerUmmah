c08e62313579ef43f31453a5fcaabaeb511cc4ac
Mais Rashed <maiss@uw.edu>
2025-01-29 11:27:04 -0800

    tabs etc.

---
+++ CyclePray/app/(tabs)/notebook.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+export function NotebookScreen() {
+    return <View style={styles.container}></View>;
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+});
