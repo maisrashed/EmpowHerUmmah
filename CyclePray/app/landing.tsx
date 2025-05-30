df141e5770c94f72828bd48f35f65a791565638d
Mais Rashed <maiss@uw.edu>
2025-05-29 21:58:39 -0700

    fixed landing page error

--- CyclePray/app/landing.tsx
+++ CyclePray/app/landing.tsx
@@ -21,8 +21,8 @@ export default function LandingScreen() {
     if (name.trim() === '') return;
     try {
       await AsyncStorage.setItem('@user_name', name);
-      await AsyncStorage.setItem('@has_launched_before', 'true'); // ✅ Set launch flag
-      navigation.navigate('(tabs src)');
+      await AsyncStorage.setItem('@has_launched_before', 'true'); 
+      navigation.navigate('(tabs src)/home');
     } catch (error) {
       console.error('Error saving name or launch status:', error);
     }
